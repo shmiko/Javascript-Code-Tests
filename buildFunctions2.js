4d890ce6fee4b59c47528cb15a4916e5ca3c5780
shmiko <shmiko@gmail.com>
2016-03-13 12:30:05 +1100

    closure

---
+++ buildFunctions2.js
@@ -0,0 +1,21 @@
+function buildFunctions(){
+	var arr = [];
+	for (var i = 0; i < 3; i++){
+		arr.push(
+			(function(j){
+				return function(){
+					console.log(j);
+				);
+		}(i));
+	}
+	return arr;
+};
+
+var fs = buildFunctions();
+
+console.log(fs[0]());
+console.log(fs[1]());
+console.log(fs[2]());
+
+//returns 3,3,3
+
