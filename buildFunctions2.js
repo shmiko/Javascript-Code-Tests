4a64f8d3582f461097de468b9a567847287b8b4a
shmiko <shmiko@gmail.com>
2016-03-13 22:35:02 +1100

    fixed stuck

--- buildFunctions2.js
+++ buildFunctions2.js
@@ -24,24 +24,6 @@ console.log(fs[2]());
 
 //returns 3,3,3
 
-function logStuff(userData) {
-    if ( typeof userData === "string")
-    {
-        console.log(userData);
-    }
-    else if ( typeof userData === "object")
-    {
-        for (var item in userData) {
-            console.log(item + ": " + userData[item]);
-        }
-    }
-};
-function getInput(options, callback) {
-	var allUserData = [];
-    allUserData.push(options);
-    callback(options);
-}
-getInput({name:"Rich", speciality:"JavaScript"}, logStuff);
 
 
 
