002eb77b610f759c5d20830f5533f7fb97e78b90
shmiko <shmiko@gmail.com>
2016-03-13 22:07:45 +1100

    callback test 2

--- callback-test.js
+++ callback-test.js
@@ -3,8 +3,12 @@ function testCallback(num,callback){
 	callback();
 };
 
-testCallback(7, callbackTest);
+testCallback(7, callbackTest2);
 
 function callbackTest(){
 	console.log('Paul - I am the callback!');
+};
+
+function callbackTest2(){
+	console.log('Yo - I am the callback!');
 };
