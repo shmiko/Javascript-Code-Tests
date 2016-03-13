c6395d13a44c3a60c6917b7f1088af67d7031d0a
shmiko <shmiko@gmail.com>
2016-03-13 22:26:28 +1100

    build js

--- buildFunctions2.js
+++ buildFunctions2.js
@@ -1,12 +1,17 @@
 function buildFunctions(){
 	var arr = [];
 	for (var i = 0; i < 3; i++){
-		arr.push(
-			(function(j){
-				return function(){
-					console.log(j);
-				);
+		arr.push(function(j){
+			return function(){
+				console.log(j);
+			}
 		}(i));
+		// arr.push(
+		// 	(function(j){
+		// 	return function(){
+		// 		console.log(j);
+		// 	}(i));
+		// });
 	}
 	return arr;
 };
--- callback-test2.js
+++ callback-test2.js
@@ -1,15 +1,9 @@
-function triangle( n ) {
-	var str = "";
-	n = parseInt(n,10);
-	for (var i = 0; i < n; i++){
-		str = str + '#';
-		console.log(str);
-	}
-};
-triangle(9);
+​(function() {
+  'use strict';
 
 
-​function logStuff(userData) {
+// generic logStuff function that prints to console​
+​function logStuff (userData) {
     if ( typeof userData === "string")
     {
         console.log(userData);
@@ -22,21 +16,25 @@ triangle(9);
 ​
     }
 ​
-};
+}
 ​
+// global variable​
 ​var allUserData;
 
 allUserData = [];
 
 ​// A function that takes two parameters, the last one a callback function​
-​function getInput(options, callback) {
+​function getInput (options, callback) {
     allUserData.push (options);
     callback (options);
 ​
-};
+}
 ​
 ​// When we call the getInput function, we pass logStuff as a parameter.​
 ​// So logStuff will be the function that will called back (or executed) inside the getInput function​
 getInput ({name:"Rich", speciality:"JavaScript"}, logStuff);
 ​//  name: Rich​
 ​// speciality: JavaScript
+
+
+}());
