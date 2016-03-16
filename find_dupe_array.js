ccade0cbb091f7419227f4f4397c8e0dec66c1c1
shmiko <shmiko@gmail.com>
2016-03-16 20:50:35 +1100

    dupe_array

---
+++ find_dupe_array.js
@@ -0,0 +1,15 @@
+function updateVegetablesCollection (veggies, veggie) {
+    if (veggies.indexOf(veggie) === -1) {
+        veggies.push(veggie);
+        console.log('New veggies collection is : ' + veggies);
+    } else if (veggies.indexOf(veggie) > -1) {
+        console.log(veggie + ' already exists in the veggies collection.');
+    }
+}
+
+var veggies = ['potato', 'tomato', 'chillies', 'green-pepper'];
+
+updateVegetablesCollection(veggies, 'spinach'); 
+// New veggies collection is : potato,tomato,chillies,green-papper,spinach
+updateVegetablesCollection(veggies, 'spinach'); 
+// spinach already exists in the veggies collection.
