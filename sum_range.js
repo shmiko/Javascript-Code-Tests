e983981e20fd531c2c5d171af1e4352d6660a6f8
shmiko <shmiko@gmail.com>
2016-03-16 21:59:01 +1100

    sumRange

---
+++ sum_range.js
@@ -0,0 +1,37 @@
+var sumRange = function(num1,num2){
+	var sumArray = [];
+	var totalSum = 0;
+	var returnObj = {};
+	for (var i = num1; i <= num2; i++){
+		sumArray.push(i);
+		totalSum += i;
+	}
+	returnObj.sumArray = sumArray;
+	returnObj.totalSum = totalSum;
+	return returnObj;
+};
+
+var runSumRange = sumRange(2,8);
+//return array with numbers ranging from 1st arg to 
+//last arg and then the sum of all
+console.log('sum range from 2 to 8 should be 35',runSumRange);
+
+var sumRangeStep = function(num1,num2,num3){
+	var sumArray = [];
+	var totalSum = 0;
+	var returnObj = {};
+	for (var i = num1; i <= num2; i=i+num3){
+		sumArray.push(i);
+		totalSum += i;
+	}
+	returnObj.sumArray = sumArray;
+	returnObj.totalSum = totalSum;
+	return returnObj;
+};
+
+var runSumRangeStep = sumRangeStep(2,8,2);
+//return array with numbers ranging from 1st arg to 
+//last arg but stepping over 3rd arg and then the sum of all
+//2,8,2 steping over each 2nd number should return an array containing
+//2,4,6,8 which in total = 20.
+console.log('sum range from 2 to 8 , step of 2 should be 20',runSumRangeStep);
