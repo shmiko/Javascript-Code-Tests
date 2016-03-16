23a40dbd89d460a0e94cf2bf92fbeafb6880ffde
shmiko <shmiko@gmail.com>
2016-03-16 22:25:11 +1100

    reverse array args

--- reverseArray.js
+++ reverseArray.js
@@ -9,3 +9,18 @@ var reverseArray = function(arr){
 
 var testReverseArray = reverseArray(['a','b','c','d']);
 console.log('reverse array should go from a,b,c,d to d,c,b,a',testReverseArray);
+
+
+//write a function to reverse the same array which is passed in to the function
+function reverseArrayArg(arr) {
+  for (var i = 0; i < Math.floor(arr.length / 2); i++) {
+    var placeHolder = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = placeHolder;
+  }
+  return arr;
+}
+
+var testReverseArrayArgs = [1, 2, 3, 4, 5];
+reverseArrayArg(testReverseArrayArgs);
+console.log(reverseArrayArg);
