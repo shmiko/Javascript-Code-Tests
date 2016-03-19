384ddad6787ac668877abc5c7dd229189bb722ed
shmiko <shmiko@gmail.com>
2016-03-19 20:50:00 +1100

    arrays

--- relationships.js
+++ relationships.js
@@ -257,6 +257,15 @@ var bracketNotation7 = true;
 var dotNotation8 = false;
 var bracketNotation8 = true;
 
+var str = 'abcdefghij';
+
+console.log('(1, 2): '   + str.substr(1, 2));   // '(1, 2): bc'
+console.log('(-3, 2): '  + str.substr(-3, 2));  // '(-3, 2): hi'
+console.log('(-3): '     + str.substr(-3));     // '(-3): hij'
+console.log('(1): '      + str.substr(1));      // '(1): bcdefghij'
+console.log('(-20, 2): ' + str.substr(-20, 2)); // '(-20, 2): ab'
+console.log('(20, 2): '  + str.substr(20, 2));  // '(20, 2): 
+
 
 var s = "audacity";
 
@@ -264,9 +273,10 @@ var udacityizer = function(s) {
     // Right now, the variable s === "audacity"
     // Manipulate s to make it equal to "Udacity"
     // Your code goes here!
-    s = s[0].toUpperCase  + s.substr(1,s.length-1);
+    s = s[0].toUpperCase;
+    var t = s.substr(1,s.length-1);
     
-    return s;
+    return s + t;
 };
 
 // Did your code work? The line below will tell you!
