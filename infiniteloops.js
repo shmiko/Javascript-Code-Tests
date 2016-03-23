4b85a4347767e0842510959711a8e260d13bea5a
shmiko <shmiko@gmail.com>
2016-03-24 05:48:55 +1100

    infinte allops

--- infiniteloops.js
+++ infiniteloops.js
@@ -609,57 +609,57 @@ swapShuffle(numbers);
 console.log('numbers are ',numbers) //[3, 4, 2, 1, 5];
 
 
-var carMaker = function(year, make, model, color, price){
-  return {
-    yeah: year,
-    make: make,
-    model: model,
-    color: color,
-    price: price
-  }
-};
+// var carMaker = function(year, make, model, color, price){
+//   return {
+//     yeah: year,
+//     make: make,
+//     model: model,
+//     color: color,
+//     price: price
+//   }
+// };
 
 
-var carDatabase = { 
-  years: [1991, 1992, 1993, 1994,1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
-  makes: ['Toyota', 'Ford', 'BMW', 'Nissan'],
-  makeAndModel: {
-    Toyota: {
-      models: ['Camry', 'Corolla', 'Prius', 'Rav4']
-    }, 
-    Ford: {
-      models: ['Fiesta', 'Focus', 'Everest', 'Mustang']
-    },
+// var carDatabase = { 
+//   years: [1991, 1992, 1993, 1994,1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
+//   makes: ['Toyota', 'Ford', 'BMW', 'Nissan'],
+//   makeAndModel: {
+//     Toyota: {
+//       models: ['Camry', 'Corolla', 'Prius', 'Rav4']
+//     }, 
+//     Ford: {
+//       models: ['Fiesta', 'Focus', 'Everest', 'Mustang']
+//     },
     
-    BMW: {
-      models: ['M3', '3 Series', '4 Series', '6 Series']
-    },
-    Nissan: {
-      models: ['Altima', 'Pathfinder', 'Leaf', 'Juke']
-    }
-  },
-  colors: ['blue', 'red', 'black', 'white', 'silver', 'champagne'],
-  prices: [20000,120,6500,8700,32000,43000,27560,200,78000]
-};
+//     BMW: {
+//       models: ['M3', '3 Series', '4 Series', '6 Series']
+//     },
+//     Nissan: {
+//       models: ['Altima', 'Pathfinder', 'Leaf', 'Juke']
+//     }
+//   },
+//   colors: ['blue', 'red', 'black', 'white', 'silver', 'champagne'],
+//   prices: [20000,120,6500,8700,32000,43000,27560,200,78000]
+// };
 
 
-var carFactory = function(carDatabase, carMaker, numberOfCars){
-  var cars = [];
-  for (var i = 0; i <= numberOfCars; i++){
-    var randomYear = carDatabase.years[Math.floor(Math.random() * carDatabase.years.length)]
-    var randomColor = carDatabase.colors[Math.floor(Math.random() * carDatabase.colors.length)];
-    var randomMake = carDatabase.makes[Math.floor(Math.random() * carDatabase.makes.length)];
-    var randomPrice = carDatabase.prices[Math.floor(Math.random() * carDatabase.prices.length)];
-    var randomModel = carDatabase.makeAndModel[randomMake] ? carDatabase.makeAndModel[randomMake]['models'][Math.floor(Math.random() * carDatabase.makeAndModel[randomMake]['models'].length)] : alert("make does not exist in database");
-    cars.push(carMaker(randomYear, randomMake, randomModel, randomColor,randomPrice));
-  }
+// var carFactory = function(carDatabase, carMaker, numberOfCars){
+//   var cars = [];
+//   for (var i = 0; i <= numberOfCars; i++){
+//     var randomYear = carDatabase.years[Math.floor(Math.random() * carDatabase.years.length)]
+//     var randomColor = carDatabase.colors[Math.floor(Math.random() * carDatabase.colors.length)];
+//     var randomMake = carDatabase.makes[Math.floor(Math.random() * carDatabase.makes.length)];
+//     var randomPrice = carDatabase.prices[Math.floor(Math.random() * carDatabase.prices.length)];
+//     var randomModel = carDatabase.makeAndModel[randomMake] ? carDatabase.makeAndModel[randomMake]['models'][Math.floor(Math.random() * carDatabase.makeAndModel[randomMake]['models'].length)] : alert("make does not exist in database");
+//     cars.push(carMaker(randomYear, randomMake, randomModel, randomColor,randomPrice));
+//   }
 
 
-  return cars;
-};
+//   return cars;
+// };
 
 
-var sampleCarList = carFactory(carDatabase, carMaker, 500);
+var sampleCarList = helpers.carFactory(helpers.carDatabase, helpers.carMaker, 500);
 console.dir(sampleCarList);
 
 // CarFax.com Sprint:
