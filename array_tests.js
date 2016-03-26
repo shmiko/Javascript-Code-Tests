340e12e038a3ae27c61eed5da63da4424b7dad18
shmiko <shmiko@gmail.com>
2016-03-26 22:51:15 +1100

    prototypes and inheritance

--- array_tests.js
+++ array_tests.js
@@ -1,1 +1,12 @@
+'use strict'
+var log = function log(value){
+	console.log(value);
+};
 //testing Prototypes and Inheritance
+var arr = ['red','blue','green'];
+
+Object.defineProperty(arr,'last',{get:function(){
+	return this[this.length-1];
+}});
+var last = arr.last;
+log(last);
