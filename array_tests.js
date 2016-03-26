eb5fd7729d205e90c8643992dca8e1f375977e7c
shmiko <shmiko@gmail.com>
2016-03-26 22:54:19 +1100

    prototypes and inheritance

--- array_tests.js
+++ array_tests.js
@@ -10,3 +10,11 @@ Object.defineProperty(arr,'last',{get:function(){
 }});
 var last = arr.last;
 log(last);
+
+//if we create a new array and try to access the last property it will be undefined
+//set the object defineProperty to the Array.prototype
+Object.defineProperty(Array.prototype,'last',{get:function(){
+	return this[this.length-1];
+}}); //this will allow all array objects to inherit the last property
+var arr2 = ['one','two','three'];
+log(arr2.last);//this works
