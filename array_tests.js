bcf42593ea8eb2ea4249457aa1d14d267bfefcca
shmiko <shmiko@gmail.com>
2016-03-26 22:59:08 +1100

    proto

--- array_tests.js
+++ array_tests.js
@@ -18,3 +18,15 @@ Object.defineProperty(Array.prototype,'last',{get:function(){
 }}); //this will allow all array objects to inherit the last property
 var arr2 = ['one','two','three'];
 log(arr2.last);//this works
+
+//var array = [] is the same as using var array = new Array[]
+//which is accessing the array prototype.
+
+//the top level prototype is object
+var myFunction = function(){};
+log(myFunction.prototype); //returns {}
+//object does not have a prototype
+var cat = {name: 'fluffy'};
+log(cat.prototype); //undefined
+
+//it does have the __proto__ preperty
