acbc97a09de03b091d9b8ebaf2a87bc3bbe00ff9
shmiko <shmiko@gmail.com>
2016-03-26 21:46:17 +1100

    object prototype ext props

--- objects_test.js
+++ objects_test.js
@@ -8,6 +8,7 @@ function House(material,type,floors,location){
 };
 
 var myHouse = new House('Brick','Duplex',2,'Windsor');
+//if you remove the new keyword the property cannot be set and returns undefined
 var yourHouse = new House('Fibro','Villa',1,'San Franisco');
 
 
@@ -23,4 +24,9 @@ log(myHouse); //returns all properties and values assigned to myHouse
 log(yourHouse); //returns all properties and values assigned to yourHouse
 log(myHouse.landSize); //returns just landsize value of myHouse
 log(myHouse['floors']); //returns just the floors value of myHouse
+log(Object.getOwnPropertyDescriptor(myHouse, 'type'));//this show the inner properties of the objext prototype
+myHouse.type = 'Flat';//sets type property
+log(myHouse.type);//displays type as Flat
+myHouse.writable = false; //disable writable property
+myHouse.type = 'Unit'; //fails to display as it cannot be written
 
