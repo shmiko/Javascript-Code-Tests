8b3ffb4e84b7c30f0c2749f531d0c960276917f4
shmiko <shmiko@gmail.com>
2016-04-08 11:20:56 +1000

    tests

--- gameLogic.js
+++ gameLogic.js
@@ -1,3 +1,4 @@
+// var _ = require("underscore"); 
 (function(){
 
 	window.gameLogic = {};
@@ -12,6 +13,26 @@
 		return square;
 	};
 
+	gameLogic.createBoard = function(num){
+		// console.log(num);
+		var board = _.range(num);
+		_.each(board,function(space,index,board){
+			row = _.map(_.range(num), function(val){
+				return gameLogic.createSquare(index.toString() + val.toString());
+			});
+			board[index] = row;
+		});
+		return board;
+		// boardArray = [];
+		// _.map(_.range(3), function(obj,key){
+		// 	_.each(boardArray.push(obj),function(key){ return key;});
+		// 	console.log('We are inside make tic tac toe',boardArray,'boardArray','key is ',key);			
+		// })
+		// return boardArray;
+		// can use _.each, _.map, _.range x 2 to solve
+		//return _.each(_.map(_.range(3, gameLogic.createGame(num))));
+		// console.log(_.each(_.map(_.range(3), gameLogic.createSquare(num){return num}),funtion(val){return val}));
+	};
 
 	gameLogic.createGame = function(){
 
@@ -36,81 +57,67 @@
 
 	
 	window.Game = gameLogic.createGame();
+	window.testBoard = Game.board;
+	window.testSquare = Game.board[0][0];
 
 // ======================== GAME LOGIC STARTS HERE ====================================
-_.each(array, function(val){ return val});
-_.map(array, function(val){ return val + '!!!'});
-_.filter(array, function(num){ return num % 2 === 0});
-_.reduce(array, function(startNum, currNum) {return startNum + currNum}, 0);
+testMakeSquare = gameLogic.createSquare(00);
+console.log('square',testMakeSquare);
 
+testMakeGame = gameLogic.createGame();
+console.log('game',testMakeGame);
+_.each(testMakeSquare, function(val){ return val});
+_.map(testMakeSquare, function(val){ return val + '!!!'});
+_.filter(testMakeSquare, function(num){ return num % 2 === 0});
+_.reduce(testMakeSquare, function(startNum, currNum) {return startNum + currNum}, 0);
+// var testRange = function repeat(times, VALUE) {
+// 	return _.map( _.range(3,gameLogic.createSquare(VALUE){
+// 		return VALUE;
+// 	}));
+// };
 
-	gameLogic.createBoard = function(num){
-		console.log(num);
-		// can use _.each, _.map, _.range x 2 to solve
-		//return _.each(_.map(_.range(3, gameLogic.createGame(num))));
-		_.each(_.map(_.range(3, gameLogic.createSquare(num)))
-	};
+	 // console.log(_.map( _.range(3),gameLogic.createSquare(num){return num;}));
+// var testRange = _.range(3,gameLogic.createSquare(00));
+// console.log(testRange);
 
-var makeArray = function(number){
-	// console.log('make array length should be ', number)
-	var num = 0;
-	var numArray = [];
-	var numberArray = [];
-	numArray.length = number;
-	loop(numArray, function(arr, ind){
-		numberArray.push(ind);
-	})
-	return numberArray;	
-};
-var testMakeArray = makeArray(10);
-console.log(testMakeArray);
-
-var i = 0;
-var makeTicTacToeBoard = function(number){
-	// boardArray.push(
-		var matrixArray = [];
-		var boardArray = [];
-		loop(makeArray(number), function(arr,index){
-			matrixArray.push(objToReplace);
-			console.log('We are inside make tic tac toe',matrixArray,'matrixArray','index is ',index);
-			
-		})
-		while (i < number){
-			boardArray.push(matrixArray ); //this dosn't work either
-			i++;
-		}
-	// )
-	return boardArray;
-}
-console.log('next function makeTicTacToeBoard  -----------------------');
-
-var testMatrix = makeTicTacToeBoard(3);
-console.log(testMatrix);
-
-var gameCreator = function(number){
-	var gameObject = {};
-	console.log('about to call makeTicTacToeBoard(number)');
-	//makeTicTacToeBoard(number);
-	gameObject.gameBoard = testMatrix;
-	//gameObject.gameBoard =  makeTicTacToeBoard(3)
-	gameObject.count = 0;
-	return gameObject;
-};
-console.log('next function gameCreator  -----------------------');
-
-var gameCreatorTest = gameCreator(3);
-console.log('gameCreator will be ',gameCreatorTest);
-
+	
+	console.log('create board',gameLogic.createBoard(3));
 
 	gameLogic.findSquare = function(board, coordinates){
 		// can use _.each, _.find, or _.filter, or _.reduce
+		var result;
+		_.each(board, function(row){
+			if (result === undefined){
+				result = _.find(row, function(square){
+					return square.coordinates === coordinates;
+				});
+			};
+		});
+		if (result === undefined){
+			result = 'Square not found';
+		}
+		return result;
 	};
+
+	console.log('find square', gameLogic.findSquare(testBoard,'00'));
 	
 
 	gameLogic.updateState = function(currentSquare, gameObject){
 		// can use _.pluck to solve
+		if (currentSquare.state === null && gameObject.turnCount % 2 === 0){
+			console.log('this is player 2\'s turn');
+			currentSquare.state = 'X';
+			gameObject.incrementTurnCount();
+		} else if (currentSquare.state === null && gameObject.turnCount % 2 === 1){
+			console.log('this is player 1\'s turn');
+			currentSquare.state = 'O';
+			gameObject.incrementTurnCount();
+		} else {
+			return 'Current square is filled';
+		}
 	};
-	
+	Game.incrementTurnCount();
+	console.log('updateState',gameLogic.updateState(testSquare,Game),testSquare);
 	
 	gameLogic.checkHorizontalWin = function(currentSquare, board){
 		// can use _.every to solve
