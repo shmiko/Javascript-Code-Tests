51e3fbd24f352517d588b298add5c651eaa6e30b
shmiko <shmiko@gmail.com>
2016-04-20 08:16:01 +1000

    array update

--- array_filter.js
+++ array_filter.js
@@ -5,6 +5,19 @@ function getStocksOver(stocks,minPrice){
 };
 
 
+//this is unused but show how the predicate is used with filter
+// Array.prototype.filter = function (predicate) {
+// 	 var results = [];
+// 	 this.forEach(function(item){
+// 	 	if (predicate(item)){
+// 	 		results.push(item);
+// 	 	}
+// 	 });
+
+
+
+// 	 return results;
+// };
 
 var expensiveStocks = getStocksOver(
 	[
