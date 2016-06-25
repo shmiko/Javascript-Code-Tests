418eec54b91c392bf319303af9118b8f3c78371c
shmiko <shmiko@gmail.com>
2016-06-25 22:38:39 +1000

    js stacks

---
+++ stack.js
@@ -0,0 +1,48 @@
+function Stack(){
+	this._top = -1;
+	this._value = [];
+};
+
+var data;
+console.log("this._top is: ",this._top);
+
+Stack.prototype.push = function(data){
+	console.log("passed in data is: ",data);
+	this._top++;
+	this._values[this._top] = data;
+};
+
+Stack.prototype.pop = function(){
+	if(this._top < 0){
+		return null;
+	}
+
+	var topElement = this._values[this._top];
+	this._top--;
+
+	this._values.length--;
+
+	return topElement;
+};
+
+Stack.prototype.peek = function(){
+	if(this._top < 0){
+		return null;
+	}
+	return this._values[this._top];
+};
+
+var stack = new Stack();
+
+console.log('The stack: ' + stack);
+
+
+for (var i = 100; i <= 300; i+=100) {
+  console.log('Pushing on stack: ' + i); 
+  stack.push(i);
+}
+
+console.log('Top of the stack using Peek: ' + stack.peek());
+
+console.log('Pop from stack. Popped element = ' + stack.pop());
+console.log('Pop from stack. Popped element = ' + stack.pop());
