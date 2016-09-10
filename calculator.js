6745e543fb828c7de10f240189934f7720ba0036
shmiko <shmiko@gmail.com>
2016-09-11 01:06:00 +1000

    calc

--- calculator.js
+++ calculator.js
@@ -419,6 +419,7 @@ var advancedPythagoreanCheck = function(int1,int2,int3){
   //I refactored the original PythagoreanCheck to be more flexible
   //using global var
   pythagorean(int1,int2,int3);
+  console.log(trueOrFalse);
   return trueOrFalse;
 };
 
