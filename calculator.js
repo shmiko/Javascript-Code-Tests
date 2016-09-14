d25064422d307b97b151d870b2f32fc3606c5b53
shmiko <shmiko@gmail.com>
2016-09-15 07:58:52 +1000

    tidy code

--- calculator.js
+++ calculator.js
@@ -428,7 +428,7 @@ var advancedPythagoreanCheck = function(int1,int2,int3){
 var truthy = advancedPythagoreanCheck(3, 4, 5);
 // console.log(truthy);
 var alsoTruthy = advancedPythagoreanCheck(5, 3, 4);
-console.log(alsoTruthy);
+// console.log(alsoTruthy);
 var falsy = advancedPythagoreanCheck(3, 4, 6);
 console.log(falsy);
 
