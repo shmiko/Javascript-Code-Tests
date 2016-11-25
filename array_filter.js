e5e2c32b83db4f299f6e99b3cbe31c4e158c7ce0
shmiko <shmiko@gmail.com>
2016-11-26 00:09:06 +1100

    ok

--- array_filter.js
+++ array_filter.js
@@ -20,7 +20,7 @@ console.log(JSON.stringify(expensiveStocks));
 
 
 function destroyer(arr) {
-  // Remove all the values reset the array
+  // Remove all the values and reset the array
 
   var newArray = [];
   var arg1;
